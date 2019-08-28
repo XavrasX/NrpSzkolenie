c6eaf86a75f6b358a233f44a4ce9ee1e647ee021
Dariusz Popis <dariusz.popis@comfuture.pl>
2019-08-28 10:13:01 +0200

    KlientMvc project added. Only controller is needed - demo

---
+++ NRP/NRP.KlientMvc/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace NRP.KlientMvc
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "OCD",
+                url: "ocd/{nr}",
+                defaults: new { controller = "Home", action = "NeverDoLikeThis" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ NRP/NRP.KlientMvc/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NRP.KlientMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public void NeverDoLikeThis(string nr)
+        {
+            var i = 0;
+            Response.Write($"<h1>Hello from Home controller</h1>{nr} {++i}<h2>OnlyControllerDemo action invoked</h2>");
+            Response.Write($"<h1>Hello from Home controller</h1>{nr} {++i}<h2>OnlyControllerDemo action invoked</h2>");
+            Response.Write($"<h1>Hello from Home controller</h1>{nr} {++i}<h2>OnlyControllerDemo action invoked</h2>");
+            Response.Write($"<h1>Hello from Home controller</h1>{nr} {++i}<h2>OnlyControllerDemo action invoked</h2>");
+            Response.Write($"<h1>Hello from Home controller</h1>{nr} {++i}<h2>OnlyControllerDemo action invoked</h2>");
+            Response.Write($"<h1>Hello from Home controller</h1>{nr} {++i}<h2>OnlyControllerDemo action invoked</h2>");
+            Response.Write($"<h1>Hello from Home controller</h1>{nr} {++i}<h2>OnlyControllerDemo action invoked</h2>");
+            Response.Write($"<h1>Hello from Home controller</h1>{nr} {++i}<h2>OnlyControllerDemo action invoked</h2>");
+            Response.Write($"<h1>Hello from Home controller</h1>{nr} {++i}<h2>OnlyControllerDemo action invoked</h2>");
+            Response.Write($"<h1>Hello from Home controller</h1>{nr} {++i}<h2>OnlyControllerDemo action invoked</h2>");
+            Response.Write($"<h1>Hello from Home controller</h1>{nr} {++i}<h2>OnlyControllerDemo action invoked</h2>");
+            Response.Write($"<h1>Hello from Home controller</h1>{nr} {++i}<h2>OnlyControllerDemo action invoked</h2>");
+            Response.Write($"<h1>Hello from Home controller</h1>{nr} {++i}<h2>OnlyControllerDemo action invoked</h2>");
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
